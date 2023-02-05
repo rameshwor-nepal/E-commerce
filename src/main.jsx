179478f38886ac12fad63b0716cb6466db046b11
rahul <rlbishwokarma@gmail.com>
2023-02-05 14:16:47 +0545

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client' 
-import Detail_page from './pages/product_detail_page/Detail_page'
-import Signin_page from './pages/signUp_signIn/Signin_page'
-  
-import Landing_page from './pages/landing_page/Landing' 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Detail_page from "./pages/product_detail_page/Detail_page";
+import Signin_page from "./pages/signUp_signIn/Signin_page";
 
+import Landing_page from "./pages/landing_page/Landing";
+import App from "./App";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Landing_page />
-  },
-  {
-    path: "about",
-    element: <Detail_page />
-  },
-  {
-    path: "signin",
-    element: <Signin_page />
-  },
-]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+root.render(
   <React.StrictMode>
-          <RouterProvider router={router} />
-
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
